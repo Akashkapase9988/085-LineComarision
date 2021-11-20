4cc07638ae77ebfea525e6665d9c5f093e0e327d
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-20 15:49:13 +0530

    [Akash adde code  for calculting length of line 1]

--- 085-LineComarision/Program.cs
+++ 085-LineComarision/Program.cs
@@ -6,7 +6,16 @@ namespace _085_LineComarision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter coordinate point for line-1");
+            Console.WriteLine("Enter coordinate point for line-1 for  X1 ||  Y2 cordiante ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter coordinate point for line-1 for  X2  || y2 cordiante ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double legth1 = Math.Sqrt(Math.Pow((x2 -x1 ), 2) + Math.Pow((y2 - y1), 2));
+
+            Console.WriteLine("Length of line one is " + "  "+ legth1);
         }
     }
 }
