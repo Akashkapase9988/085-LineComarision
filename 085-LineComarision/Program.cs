b8212cded15ebbf561aa7c863c98925a2ff3187d
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-20 16:03:55 +0530

    [akash added code for eqal to method for checking both line sre eqal or not]

--- 085-LineComarision/Program.cs
+++ 085-LineComarision/Program.cs
@@ -16,6 +16,23 @@ namespace _085_LineComarision
             double legth1 = Math.Sqrt(Math.Pow((x2 -x1 ), 2) + Math.Pow((y2 - y1), 2));
 
             Console.WriteLine("Length of line one is " + "  "+ legth1);
+
+            
+            Console.WriteLine("-------------------------For  length of line 2-----------------------------");
+            
+            
+            Console.WriteLine("Enter coordinate point for line-2");
+            Console.WriteLine("Enter coordinate point for line-1 for  A1 ||  B1 cordiante ");
+            int A1 = Convert.ToInt32(Console.ReadLine());
+            int B1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter coordinate point for line-1 for  A2  || B2 cordiante ");
+            int A2 = Convert.ToInt32(Console.ReadLine());
+            int B2 = Convert.ToInt32(Console.ReadLine());
+            double legth2 = Math.Sqrt(Math.Pow((A2 - A1), 2) + Math.Pow((B2 - B1), 2));
+
+            Console.WriteLine("Length of line one is " + "  " + legth2);
+
+            Console.WriteLine(legth1.Equals(legth2));
         }
     }
 }
