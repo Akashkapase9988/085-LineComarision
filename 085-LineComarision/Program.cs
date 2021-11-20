675c2daee245158d9e4310843b54fc9c6c777f48
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-20 15:34:22 +0530

    []akash added welcome message to welcome branch

--- 085-LineComarision/Program.cs
+++ 085-LineComarision/Program.cs
@@ -6,7 +6,7 @@ namespace _085_LineComarision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("welcome to line comarision problem");
         }
     }
 }
